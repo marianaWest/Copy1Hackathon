f98211379f56502ea8a9fadd1308d63e3f96c8a2
Mariana West Hartenthal <97693233+marianaWest@users.noreply.github.com>
2025-11-05 17:40:46 +0000

    creates list of female by honorifics)

--- genderDetector.py
+++ genderDetector.py
@@ -3,6 +3,8 @@ import json
 
 detector = gender.Detector()
 identification_keys = ["Copyright owner of work","Copyright author of work",  "Photographer",  "Photographer(s)", "Copyright owner and author of work", "Copyright owner(s) and author(s) of work"]
+female_honorifics = ["miss", "mrs", "ms", "lady", "madame", "dame", "madam"]
+female_by_honorific = []
 
 
 def detect_gender(name):
@@ -15,9 +17,18 @@ with open("data_parsed.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
 # remove items without reference to photographer - might move somewhere else
-no_photographer = [
+with_photographer = [
     item for item in data
     if any(key in item for key in identification_keys)
 ]
 
-print((len(no_photographer)))
+# checks for honorifics
+for item in with_photographer:
+    for key in identification_keys:
+        if key in item and isinstance(item[key], str):
+            text = item[key].lower()
+            if any(title in text for title in female_honorifics):
+                female_by_honorific.append(item)
+                break
+
+print(len(female_by_honorific))
