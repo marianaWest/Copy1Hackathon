8f4a231bc02226b30d02e26a2db93afbe86524da
Mariana West Hartenthal <97693233+marianaWest@users.noreply.github.com>
2025-11-05 19:01:01 +0000

    adds gender by honorific to a json file

--- genderDetector.py
+++ genderDetector.py
@@ -3,15 +3,9 @@ import json
 
 detector = gender.Detector()
 identification_keys = ["Copyright owner of work","Copyright author of work",  "Photographer",  "Photographer(s)", "Copyright owner and author of work", "Copyright owner(s) and author(s) of work"]
-female_honorifics = ["miss", "mrs", "ms", "lady", "madame", "dame", "madam"]
+female_honorifics = ["miss ", "miss.", "mrs","ms. ", "lady", "madame", "dame ", "madam"]
 female_by_honorific = []
-
-
-def detect_gender(name):
-    print(detector.get_gender(name))
-
-result = detect_gender('Mrs')
-print(result)
+gender_by_name = []
 
 with open("data_parsed.json", "r", encoding="utf-8") as f:
     data = json.load(f)
@@ -21,6 +15,14 @@ with_photographer = [
     item for item in data
     if any(key in item for key in identification_keys)
 ]
+
+def detect_gender(name):
+    print(detector.get_gender(name))
+
+result = detect_gender('Mrs')
+print(result)
+
+
 
 # checks for honorifics
 for item in with_photographer:
@@ -32,3 +34,6 @@ for item in with_photographer:
                 break
 
 print(len(female_by_honorific))
+
+with open("female_by_honorific", "w", encoding="utf-8") as f:
+    json.dump(female_by_honorific, f, indent=4, ensure_ascii=False)
