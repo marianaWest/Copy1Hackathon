34343033614166d372b11425cdd4738cbe24e797
Mariana West Hartenthal <97693233+marianaWest@users.noreply.github.com>
2025-11-10 11:53:34 +0000

    gets list of unknown photographers

--- gpt.py
+++ gpt.py
@@ -1,15 +1,31 @@
 from dotenv import load_dotenv
 import os
 from openai import OpenAI
+import json
 
 load_dotenv()
 GPTKEY = os.getenv('GPTKEY')
 
 client = OpenAI(api_key=GPTKEY)
 
+with open("undefined.json", "r", encoding="utf-8") as f:
+    undefined = json.load(f)
+
+
 response = client.responses.create(
     model="gpt-3.5-turbo",
-    input="Write a one-sentence bedtime story about a unicorn."
+       input=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "input_text",
+                   "text": f"Here is a list:\n{undefined}\nHow many items does it have?",
+                },
+               
+            ]
+        }
+    ]
 )
 
 print(response.output_text)
