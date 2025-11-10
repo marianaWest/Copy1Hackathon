f9cac57710ad46f84b257f6d3c0525cb524e1aa9
Mariana West Hartenthal <97693233+marianaWest@users.noreply.github.com>
2025-11-10 11:44:15 +0000

    creates openAi client

--- gpt.py
+++ gpt.py
@@ -1,7 +1,15 @@
 from dotenv import load_dotenv
 import os
+from openai import OpenAI
 
 load_dotenv()
+GPTKEY = os.getenv('GPTKEY')
 
-GPT_KEY = os.getenv('GPT_KEY')
+client = OpenAI(api_key=GPTKEY)
 
+response = client.responses.create(
+    model="gpt-3.5-turbo",
+    input="Write a one-sentence bedtime story about a unicorn."
+)
+
+print(response.output_text)
